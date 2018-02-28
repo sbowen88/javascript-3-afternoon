8b222d4fa149c7f0896f2ebad75cb7c213ad39f0
sbowen88 <sbowey@gmail.com>
2018-02-28 15:21:15 -0700

    First part finished

--- classes.js
+++ classes.js
@@ -30,8 +30,23 @@
 */
 
 //Code Here
+class Employee{
+    constructor(first_name, last_name, email, age){
+      this.first_name= first_name;
+      this.last_name = last_name;
+      this.email = email;
+      this.age = age
+    }
 
-
+    makeWidget (){
+      return `${this.first_name} ${this.last_name} Widget`;
+    }
+    
+}
+ let chris = new Employee('Chris', 'Johnson', 'chrisjohnson@gmail.com', 25);
+ chris 
+ let chrisWidget = chris.makeWidget();
+ chrisWidget
 
 ////////// PROBLEM 2 //////////
 
@@ -50,8 +65,29 @@
 */
 
 //Code Here
-
-
+class Manager{
+     constructor(first_name, last_name, email, age, reports){
+      this.first_name= first_name;
+      this.last_name = last_name;
+      this.email = email;
+      this.age = age
+      this.reports = [];
+    }
+    makeWidget (){
+      return `${this.first_name} ${this.last_name} widget`;
+    }
+    hire(newEmployee){
+        this.reports.push(newEmployee);
+    }
+    
+    fire(index){
+        this.reports.splice(index,1);
+    }
+}
+let Mark = new Manager('Mark', 'Williams', 'mw@gmail.com', 40, [])
+Mark
+let markWidget = Mark.makeWidget();
+markWidget
 
 ////////// PROBLEM 3 //////////
 
@@ -76,6 +112,52 @@
 */
 
 //Code Here
+class ProgressiveManager{
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+   this.first_name= first_name;
+   this.last_name = last_name;
+   this.email = email;
+   this.age = age
+   this.reports = [];
+   this.title = 'Not a manager';
+   this.bonus = 0;
+ }
+ update(){
+  if(this.reports.length === 0){
+    this.title = 'Not Manager';
+  }
+  else if (1 <=this.reports.length && this.reports.length <= 3){
+    this.title = 'Barely Manager';
+  }
+  else if (4 <=this.reports.length && this.reports.length <= 10){
+    this.title = 'Mostly Manager';
+  }
+  else if (11<=this.reports.length && this.reports.length<=40){
+    this.title = 'Manager';
+  }
+  else if (51 <=this.reports.length && this.reports.length <=100){
+    this.title = 'Manager Plus';
+  }
+  else{
+    this.title = 'Bestest Manager';
+  }
+}
+
+ hire(newEmployee){
+  this.reports.push(newEmployee);
+  this.update();
+  
+}
+fire(index){
+  this.reports.splice(index,1);
+  this.update();
+  this.bonus+= 100;
+}
+
+
+
+}
+
 
 
 
@@ -104,4 +186,27 @@
 
 //Code Here
 
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+  makeWidgets(number){
+    this.widgets_made_count+= number;
+    this.wear_and_tear_count+= Math.floor(number/50);
+  }
+  reboot(){
+    this.wear_and_tear_count -=10;
+    this.needs_reboot = false;
+    return function(){
+
+    }
+  }
+
+
+}
 
